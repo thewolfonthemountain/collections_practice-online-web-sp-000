e1259ce4d73ead11771db3a17c09f04276806558
Sean Langton <slangton1@gmail.com>
2018-10-30 02:58:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  new_array = [array[0],array[2],array[1]]
+end
+
+def reverse_array(array)
+  array.sort do |a,b|
+    "b" <=> "a"
+  end
+end
+
+def kesha_maker(array)
+array.each{|word| word[2]="$"}
+end
+
+def find_a(array)
+  array.select{|word| word[0]=="a"}
+end
+
+def sum_array(array)
+  i = 0
+  array.each{|integer| i = i+integer}
+  i
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element,index| if index != 1 then "#{element}s" else element end}
+  end
